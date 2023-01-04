3b164637fd1fa4acb433a4d68d9c4f45fefc74f0
Sia9498 <asees9498@gmail.com>
2023-01-04 07:34:35 -0600

    new changes

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,20 @@
 from statistics import mode
 from django.db import models
+import random as rand
 
 # Create your models here.
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(blank=True, null=True, upload_to='images/')
+
+    class Meta:
+        ordering = ["-id"]
+        
+    def serialize(self):
+        return {
+            "id" : self.id, 
+            "content" : self.content, 
+            "likes": rand.randint(0, 200)
+        }
 
